78fd9492db137fab5cce104f7859adaf05c6f977
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-09-26 01:41:31 +0300

    export todos to a json file

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Exports a json file with a to-do list information for a given employee ID."""
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    id = sys.argv[1]
+
+    user = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/?id={id}"
+    ).json()[0]
+    todos = requests.get(
+        f"https://jsonplaceholder.typicode.com/todos/?userId={id}"
+    ).json()
+
+    with open(f"{id}.json", "w") as json_file:
+        json.dump({id: [{
+                "task": todo.get("title"),
+                "completed": todo.get("completed"),
+                "username": user.get("username")
+            } for todo in todos]}, json_file)
