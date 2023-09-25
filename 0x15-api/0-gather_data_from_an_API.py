1fc5ed591560d19ae42828c6f0de495dd8987c64
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-09-26 01:21:03 +0300

    lock module

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,25 +4,26 @@ import json
 import requests
 import sys
 
-id = sys.argv[1]
+if __name__ == "__main__":
+    id = sys.argv[1]
 
-user = requests.get(f"https://jsonplaceholder.typicode.com/users/?id={id}")
-todos = requests.get(
-    f"https://jsonplaceholder.typicode.com/todos/?userId={id}"
-)
+    user = requests.get(f"https://jsonplaceholder.typicode.com/users/?id={id}")
+    todos = requests.get(
+        f"https://jsonplaceholder.typicode.com/todos/?userId={id}"
+    )
 
-user = json.loads(user.text)
-todos = json.loads(todos.text)
+    user = json.loads(user.text)
+    todos = json.loads(todos.text)
 
-NUMBER_OF_DONE_TASKS = 0
-TOTAL_NUMBER_OF_TASKS = len(todos)
+    NUMBER_OF_DONE_TASKS = 0
+    TOTAL_NUMBER_OF_TASKS = len(todos)
 
-for todo in todos:
-    if todo.get("completed"):
-        NUMBER_OF_DONE_TASKS += 1
+    for todo in todos:
+        if todo.get("completed"):
+            NUMBER_OF_DONE_TASKS += 1
 
-print(f"Employee {user[0].get('name')} is done "
-      "with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
-for todo in todos:
-    if todo.get("completed"):
-        print(f"\t {todo.get('title')}")
+    print(f"Employee {user[0].get('name')} is done "
+          "with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+    for todo in todos:
+        if todo.get("completed"):
+            print(f"\t {todo.get('title')}")
