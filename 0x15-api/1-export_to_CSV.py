f18583817cc6c513d9a793e98210e6a8b6554d25
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-09-26 01:32:12 +0300

    export todos to a csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Exports a csv with a to-do list information for a given employee ID."""
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    id = sys.argv[1]
+
+    user = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/?id={id}"
+    ).json()[0]
+    todos = requests.get(
+        f"https://jsonplaceholder.typicode.com/todos/?userId={id}"
+    ).json()
+
+    with open(f"{user.get('id')}.csv", "w", newline="") as csv_file:
+        writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+
+        for todo in todos:
+            writer.writerow([user.get("id"),
+                             user.get("username"),
+                             todo.get("completed"),
+                             todo.get("title")])
